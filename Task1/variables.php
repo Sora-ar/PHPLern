b2675ee54a9a29b04636f28bc0f2c14113bdc628
Alexandra <atamanenkoa@school73kharkov.com>
2024-09-19 00:07:56 +0300

    add variable type check

--- Task1/variables.php
+++ Task1/variables.php
@@ -3,4 +3,9 @@ $string_value = 'Bob';
 $int_value = 123;
 $float_value = 12.34;
 $bool_value = true;
-echo "$string_value \n $int_value \n $float_value \n $bool_value";
+echo "$string_value \n $int_value \n $float_value \n $bool_value \n";
+
+var_dump($string_value);
+var_dump($int_value);
+var_dump($float_value);
+var_dump($bool_value);
